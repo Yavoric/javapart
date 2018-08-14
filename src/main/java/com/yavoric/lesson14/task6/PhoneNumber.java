b16af723fb5c5e277670ffc93394bdb2d3c83732
Yavoric <yavoric@gmail.com>
2018-08-14 23:09:29 +0300

    112

--- src/main/java/com/yavoric/lesson14/task6/PhoneNumber.java
+++ src/main/java/com/yavoric/lesson14/task6/PhoneNumber.java
@@ -2,14 +2,15 @@ package com.yavoric.lesson14.task6;
 
 public class PhoneNumber {
     private String[] number = {"0","1","2","3","4","5","6","7","8","9"};
-    private String elementaryPhnoneNumber = "380";
+    private String elementaryPhnoneNumber = "999";
     private String fullPhnoneNumber;
     public String getFullPnoneNumber(){
         fullPhnoneNumber = elementaryPhnoneNumber;
-        for (int i =0; i<9;i++){
+        for (int i =0; i<8;i++){
             int c = (int) (Math.random()* 9);
             fullPhnoneNumber = fullPhnoneNumber+number[c];
         }
+        fullPhnoneNumber = fullPhnoneNumber+"5";
         return fullPhnoneNumber;
     }
 
