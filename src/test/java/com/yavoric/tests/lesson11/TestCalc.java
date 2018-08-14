ec460e0ed4ebd8f80bb141c04a9d1d2cf5371f77
Yavoric <yavoric@gmail.com>
2018-08-14 23:39:04 +0300

    gg

---
+++ src/test/java/com/yavoric/tests/lesson11/TestCalc.java
@@ -0,0 +1,14 @@
+package com.yavoric.tests.lesson11;
+import com.yavoric.lesson11.CalcMain;
+import com.yavoric.lesson11.Operation;
+public class TestCalc {
+    public static void main(String[] args) {
+        double[] arr1 = {1, 2, 3, 4, 5};
+        Operation x = new Operation();
+        if (x.Addition() ==15) {
+
+            System.out.println("Test Sucses");
+
+        }
+    }
+}
